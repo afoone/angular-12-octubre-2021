b38db1e9ed810c3cbdede265e3334e47ce0e527e
Alfonso Tienda <afoone@hotmail.com>
2021-10-22 14:38:51 +0200

    pipes testing

--- 05-pipes/patients/src/app/app.module.ts
+++ 05-pipes/patients/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './components/patient/patient-list/patient-list.component';
+import { MalesPipe } from './pipes/males.pipe';
+import { SumaPipe } from './pipes/suma.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent,
+    MalesPipe,
+    SumaPipe
   ],
   imports: [
     BrowserModule,
