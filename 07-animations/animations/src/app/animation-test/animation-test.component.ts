f742ad2e7db173ee4d5d3c37a581ed45f4e63eaa
Alfonso Tienda <afoone@hotmail.com>
2021-10-27 10:11:43 +0200

    animations

--- 07-animations/animations/src/app/animation-test/animation-test.component.ts
+++ 07-animations/animations/src/app/animation-test/animation-test.component.ts
@@ -1,23 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  state,
+  transition,
+  trigger,
+  style,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-animation-test',
   templateUrl: './animation-test.component.html',
-  styleUrls: ['./animation-test.component.css']
+  styleUrls: ['./animation-test.component.css'],
+  animations: [
+    trigger('open-close', [
+      // transition('open=>close', animate('0.5s')),
+      // transition('close=>open', animate('1s')),
+      // transition('*=>close', animate('0.5s')),
+      // transition('*=>open', animate('1s')),
+      transition('open<=>close', animate('2s 0ms ease-out')),
+      state(
+        'open',
+        style({
+          padding: '0.3rem',
+          opacity: 1,
+          backgroundColor: 'yellow',
+          transform: 'translateX(100px)'
+        })
+      ),
+      state(
+        'close',
+        style({
+          height: '1.2rem',
+          opacity: 0.5,
+          backgroundColor: 'blue',
+          color: 'white',
+          transform: 'translateX(200px)'
+        })
+      ),
+    ]),
+  ],
 })
 export class AnimationTestComponent implements OnInit {
-
   public open: boolean;
 
   constructor() {
-    this.open = false
-   }
-
-  ngOnInit(): void {
+    this.open = false;
   }
+
+  ngOnInit(): void {}
 
   toggle = () => {
-    this.open = !this.open
-  }
-
+    this.open = !this.open;
+  };
 }
